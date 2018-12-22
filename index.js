5a151a68d3708938aa656f3f1c194e426bef5957
Holden Altaffer <haltaffer@gmail.com>
2018-12-22 04:20:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,14 +26,14 @@ function deepestChild() {
 
 function increaseRankBy(n) {
   const rankedLists = document.querySelectorAll( '.ranked-list' )
-  const firstList = rankedLists[0]
-  const secondList = rankedLists[1]
+  // const firstList = rankedLists[0]
+  // const secondList = rankedLists[1]
 
-  let children1 = firstList.children
-
-  for ( let i = 0, l = children1.length; i < l; i++ ) {
-    children1[ i ].innerHTML = parseInt(children1[ i ].innerHTML) + n;
-  }
+  // let children1 = firstList.children
+  // 
+  // for ( let i = 0, l = children1.length; i < l; i++ ) {
+  //   children1[ i ].innerHTML = parseInt(children1[ i ].innerHTML) + n;
+  // }
 
   // let children2 = secondList.children
   //
@@ -41,5 +41,5 @@ function increaseRankBy(n) {
   // for ( let i = 0, l = children2.length; i < l; i++ ) {
   //   children2[ i ].innerHTML = parseInt(children2[ i ].innerHTML) + n;
   // }
-  console.log(secondList.children[3].innerHTML);
+  console.log(rankedLists.length);
 }
