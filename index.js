e8e276bd01176f62ff4eea09a0dae67dd4fcb8da
Holden Altaffer <haltaffer@gmail.com>
2018-12-22 02:47:34 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,19 @@
+// const getFirstSelector = function() {
+//   return document.querySelector('div');
+// }
+
+function getFirstSelector(selectors) {
+  return document.querySelector(selectors);
+}
+
+const lis = document
+  .getElementById('app')
+  .querySelectorAll('ul.ranked-list li');
+
+for (let i = 0; i < lis.length; i++) {
+  lis[i].innerHTML = (i + 1).toString();
+}
+
+function nestedTarget() {
+  return document.getElementsByTagName('div')[0];
+}
