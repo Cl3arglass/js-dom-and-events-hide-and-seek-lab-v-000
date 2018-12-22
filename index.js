5bc5aff0b581b851d2b2b49f4bd777a87168608a
Holden Altaffer <haltaffer@gmail.com>
2018-12-22 03:34:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -39,6 +39,6 @@ function increaseRankBy(n) {
 
 
   for ( let i = 0, l = children2.length; i < l; i++ ) {
-    children2[ i ].innerHTML = parseInt(children2[ i ].innerHTML) + n;
+    children2[ i ].innerHTML = parseInt(children2[ i ].innerHTML);
   }
 }
