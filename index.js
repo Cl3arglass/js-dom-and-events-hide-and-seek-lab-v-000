beab9158752494356e3e1a68518d09271ba10eac
Holden Altaffer <haltaffer@gmail.com>
2018-12-22 03:23:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,3 +23,22 @@ function nestedTarget() {
 function deepestChild() {
   return  document.querySelectorAll('div#grand-node div')[3];
 }
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll( '.ranked-list' )
+  const firstList = rankedLists[ 0 ]
+  const secondList = rankedLists[ 1 ]
+
+  let children1 = firstList.children
+  let start = 1
+  for ( let i = 0, l = children1.length; i < l; i++ ) {
+    children1[ i ].innerHTML = parseInt(i + n)
+  }
+
+  children2 = secondList.children
+  start = 12
+
+  for ( let i = 0, l = children2.length; i < l; i++ ) {
+    children2[ i ].innerHTML = parseInt(i + n)
+  }
+}
