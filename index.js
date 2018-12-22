104959b75b25925277d17d56bc6e59aa67f8c8b2
Holden Altaffer <haltaffer@gmail.com>
2018-12-22 04:28:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,20 +26,20 @@ function deepestChild() {
 
 function increaseRankBy(n) {
   const rankedLists = document.getElementById('app').querySelectorAll( 'ul.ranked-list li' )
-  // const firstList = rankedLists[0]
-  // const secondList = rankedLists[1]
+  const firstList = rankedLists[0]
+  const secondList = rankedLists[1]
 
-  // let children1 = firstList.children
-  //
-  // for ( let i = 0, l = children1.length; i < l; i++ ) {
-  //   children1[ i ].innerHTML = parseInt(children1[ i ].innerHTML) + n;
-  // }
+  let children1 = firstList.children
 
-  // let children2 = secondList.children
-  //
-  //
-  // for ( let i = 0, l = children2.length; i < l; i++ ) {
-  //   children2[ i ].innerHTML = parseInt(children2[ i ].innerHTML) + n;
-  // }
+  for ( let i = 0, l = children1.length; i < l; i++ ) {
+    children1[ i ].innerHTML = parseInt(children1[ i ].innerHTML) + n;
+  }
+
+  let children2 = secondList.children
+
+
+  for ( let i = 0, l = children2.length; i < l; i++ ) {
+    children2[ i ].innerHTML = parseInt(children2[ i ].innerHTML) + n;
+  }
   // console.log(rankedLists[1].children[1].innerHTML);
 }
